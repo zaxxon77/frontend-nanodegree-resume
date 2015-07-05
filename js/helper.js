17231788031bc125d12f124b6e00a820e47df27d
Zack Schmid <zaxxon77@gmail.com>
2015-07-05 13:51:52 -0500

    Initial commit

--- js/helper.js
+++ js/helper.js
@@ -13,21 +13,21 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
+var HTMLheaderRole = '<span class="white-text">%data%</span><hr/>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item"><span class="lt-blue-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="lt-blue-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="lt-blue-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="lt-blue-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="lt-blue-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="lt-blue-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="lt-blue-text">location</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskills = '<li class="flex-box"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
@@ -40,7 +40,7 @@ var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<img src="%data%" class="img_project">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
@@ -51,7 +51,7 @@ var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineSchool = ' -- %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
@@ -85,7 +85,7 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  logClicks(loc.pageX,loc.pageY);
 });
 
 
@@ -171,6 +171,7 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      infoWindow.open(map,marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -233,11 +234,13 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
+
+
---
+++ js/resumeBuilder.js
@@ -0,0 +1,256 @@
+
+var bio = {
+	"name" : "ZACK SCHMID", 
+	"role" : "AEROSPACE ENGINEER, ENTREPRENEUR, ASPIRING WEB DEVELOPER", 
+	"contacts" : {
+		"mobile" : "(512) 766-4711",
+		"email" : "zaxxon77@gmail.com",
+		"github" : "zaxxon77",
+		"twitter" : "too old for that shit",
+		"location" : "Austin, TX"
+	},
+	"welcomeMessage" : "Thirteen years of Mechanical, Aeronautical, Aerospace, and Systems Engineering experience with a strong background spanning leadership and program planning, missile system navigation, guidance, and control (NG&C) design and analysis, flight software development, physics-based model and simulation development, real time hardware-in-the-loop testing (HWIL), and system/subsystem/product requirements development, verification, and validation. Experience with total product lifecycle from design to deployment. Consistent top performer, and Subject Matter Expert in boost-phase missile stability. Recent experience creating and running a small trucking business. Currently enrolled in Udacity Front End Web Developmer Nanodegree program. Always looking to learn, grow, and develop my skill set.",
+	"skills" : ["Project Management", "Team Building", "Microsoft Office", "Windows", "Unix/Linux", "Pumping it Up", "Keeping it Real", "C/C++", "Fortran", "Matlab/Simulink", "Autocad"],
+	"bioPic" : "images/ProfilePic.jpg"
+}
+
+bio.display = function (){
+	$("#header").prepend(HTMLheaderRole.replace("%data%",bio.role));
+	$("#header").prepend(HTMLheaderName.replace("%data%",bio.name));
+
+	$("#topContacts").append(HTMLmobile.replace("%data%",bio.contacts.mobile));
+	$("#topContacts").append(HTMLemail.replace("%data%",bio.contacts.email));
+	//$("#header").append(HTMLtwitter.replace("%data%",bio.contacts.twitter));
+	$("#topContacts").append(HTMLgithub.replace("%data%",bio.contacts.github));
+	//$("#header").append(HTMLblog.replace("%data%",bio.contacts.blog));
+	$("#topContacts").append(HTMLlocation.replace("%data%",bio.contacts.location));
+
+	$("#header").append(HTMLbioPic.replace("%data%",bio.bioPic));
+	$("#header").append(HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage));
+
+	if (bio.skills.length > 0) {
+		$("#header").append(HTMLskillsStart);
+		for (skill in bio.skills) {
+			$("#skills").append(HTMLskills.replace("%data%",bio.skills[skill]));
+		}
+	}
+}
+bio.display();
+
+footerContacts.display = function () {
+	$("#footerContacts").append(HTMLmobile.replace("%data%",bio.contacts.mobile));
+	$("#footerContacts").append(HTMLemail.replace("%data%",bio.contacts.email));
+	//$("#header").append(HTMLtwitter.replace("%data%",bio.contacts.twitter));
+	$("#footerContacts").append(HTMLgithub.replace("%data%",bio.contacts.github));
+	//$("#header").append(HTMLblog.replace("%data%",bio.contacts.blog));
+	$("#footerContacts").append(HTMLlocation.replace("%data%",bio.contacts.location));
+}	
+footerContacts.display();
+
+var work = {
+	"jobs": [
+	{
+		"employer": "Sequoia Trucking LLC",
+		"title": "Owner",
+		"location": "Austin, TX",
+		"dates": "Feb 2014 - current",
+		"description": "Established a long-haul semi-trucking business, set up as a freight carrier. Sequoia Trucking LLC owns and leases trucks to a broker for load scheduling, and employs CDL licensed operators.",
+		"url": "https://www.facebook.com/SequoiaTrucking"
+	},
+	{
+		"employer": "Lockheed Martin Space Systems Company",
+		"title": "Staff Software Engineer on FBM",
+		"location": "Sunnyvale, CA",
+		"dates": "Feb 2009 - Aug 2013",
+		"description": "Missile Avionics Life Extension Program Planning Team Lead, Missile NG&C Lead, Subject Matter Expert",
+		"url": "http://www.lockheedmartin.com/us/products/trident-ii-d5-fleet-ballistic-missile--fbm-.html"
+	},
+	{
+		"employer": "Lockheed Martin Space Systems Company",
+		"title": "Software Engineer on THAAD",
+		"location": "Sunnyvale, CA",
+		"dates": "July 2000 - Jan 2009",
+		"description": "Lead Autopilot Controls and Plant Estimates Analyst, Certified Principal Engineer for missile flight software builds",
+		"url": "http://www.lockheedmartin.com/us/products/thaad.html"
+	}
+	]
+}
+
+function displaywork (){
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+		var formattedEmployerURL = formattedEmployer.replace("#",work.jobs[job].url);
+		var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployerURL + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+
+		var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDates);
+
+		var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+	}
+}
+displaywork();
+
+
+var education = {
+	"schools" : [ 
+	{
+		"name": "San Jose State University",
+		"location": "San Jose, CA",
+		"degree": "Masters",
+		"majors": "Aerospace Engineering",
+		"date": 2005,
+		"url": "http://www.sjsu.edu/"
+	},
+	{
+		"name": "University of California at Davis",
+		"location": "Davis, CA",
+		"degree": "BS",
+		"majors": "Mechanical and Aeronautical Engineering",
+		"date": 2000,
+		"url": "http://ucdavis.edu/"
+	},
+	{
+		"name": "Santa Clara University",
+		"location": "Santa Clara, CA",
+		"degree": "coursework",
+		"majors": "Linear, Non-linear, and Digital Control Theory",
+		"date": 2003,
+		"url": "http://scu.edu/ecp/"
+	}
+	],
+	"onlineCourses": [
+	{
+		"title": "Intro to HTML and CSS",
+		"school": "Udacity",
+		"date": 2015,
+		"url": "https://www.udacity.com/course/intro-to-html-and-css--ud304"
+	},
+	{
+		"title": "Responsive Web Design Fundamentals",
+		"school": "Udacity",
+		"date": 2015,
+		"url": "https://www.udacity.com/course/responsive-web-design-fundamentals--ud893"
+	},
+	{
+		"title": "Responsive Images",
+		"school": "Udacity",
+		"date": 2015,
+		"url": "https://www.udacity.com/course/responsive-images--ud882"
+	},
+	{
+		"title": "JavaScript Basics",
+		"school": "Udacity",
+		"date": 2015,
+		"url": "https://www.udacity.com/course/javascript-basics--ud804"
+	},
+	{
+		"title": "Intro to jQuery",
+		"school": "Udacity",
+		"date": 2015,
+		"url": "https://www.udacity.com/course/intro-to-jquery--ud245"
+	}
+	]
+}
+
+education.display = function (){
+	for (school in education.schools) {
+		$("#education").append(HTMLschoolStart);
+
+		var formattedName = HTMLschoolName.replace("%data%",education.schools[school].name);
+		var formattedNameAndURL = formattedName.replace("#",education.schools[school].url);
+		$(".education-entry:last").append(formattedNameAndURL);
+
+		var formattedDegree = HTMLschoolDegree.replace("%data%",education.schools[school].degree);
+		$(".education-entry:last").append(formattedDegree);
+
+		var formattedDates = HTMLschoolDates.replace("%data%",education.schools[school].date);
+		$(".education-entry:last").append(formattedDates);
+
+		var formattedLocation = HTMLschoolLocation.replace("%data%",education.schools[school].location);
+		$(".education-entry:last").append(formattedLocation);
+
+		var formattedMajor = HTMLschoolMajor.replace("%data%",education.schools[school].majors);
+		$(".education-entry:last").append(formattedMajor);
+
+	}
+	$(".education-entry:last").append(HTMLonlineClasses);
+	for (course in education.onlineCourses) {
+		var formattedTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[course].title);
+		var formattedTitleAndURL = formattedTitle.replace("#",education.onlineCourses[course].url)
+		$(".education-entry:last").append(formattedTitleAndURL);
+		$(".education-entry:last").append(HTMLonlineSchool.replace("%data%",education.onlineCourses[course].school));
+		$(".education-entry:last").append(HTMLonlineDates.replace("%data%",education.onlineCourses[course].date));
+		//$(".education-entry:last").append(HTMLonlineURL.replace("%data%",education.onlineCourses[course].url));
+	}	
+}
+education.display();
+
+
+var projects = {
+	"projects" : [
+	{
+		"title": "Project 1: Udacity Portfolio Website",
+		"description": "Created a responsive website which matched the layout of an example portfolio, and re-styled it for personal use.  Extensive use of HTML, Bootstrap CSS framework, and responsive design fundamentals.",
+		"date": 2015,
+		"images": "images/Project1_screenshot.png",
+		"url": "https://github.com/zaxxon77"
+	},
+	{
+		"title": "Project 2: Resume Website",
+		"description": "Created a personalized website to showcase my personal resume.  Use of jQuery’s DOM manipulation to dynamically populate resume fields. Use of JavaScript, jQuery, as well as version control using git and GitHub.",
+		"date": 2015,
+		"images": "images/TPS_report.jpg",
+		"url": "https://github.com/zaxxon77/frontend-nanodegree-resume"
+	}
+	]
+}
+projects.display = function (){
+	for (project in projects.projects) {
+		$("#projects").append(HTMLprojectStart);
+
+		var formattedProjectTitle = HTMLprojectTitle.replace("%data%",projects.projects[project].title);
+		var formattedProjectTitleURL = formattedProjectTitle.replace("#",projects.projects[project].url);
+		$(".project-entry:last").append(formattedProjectTitleURL);
+
+		var formattedProjectDates = HTMLprojectDates.replace("%data%",projects.projects[project].date);
+		$(".project-entry:last").append(formattedProjectDates);
+
+		var formattedProjectDescription = HTMLprojectDescription.replace("%data%",projects.projects[project].description);
+		$(".project-entry:last").append(formattedProjectDescription);
+
+		var formattedProjectImage = HTMLprojectImage.replace("%data%",projects.projects[project].images);
+		$(".project-entry:last").append(formattedProjectImage);
+	}
+}
+projects.display();
+
+
+function inName (name) {
+    var finalName = name;
+    var space_index = finalName.indexOf(" ");
+    var first_let = finalName[0].toUpperCase();
+    var first_name = finalName.slice(1, space_index);
+    first_name = first_let+first_name.toLowerCase();
+    var last_name = finalName.slice(space_index+1,finalName.length).toUpperCase();
+    finalName = first_name + " " + last_name;
+    return finalName;
+}
+//console.log(inName(name));
+
+
+//$("#main").append(internationalizeButton);
+
+
+// Lets see a map!
+$("#mapDiv").append(googleMap);
+
+
+// D3.js sandbox
+//d3.select("body").transition()
+//    .style("background-color", "gray");
+
+
